4a9d3b3851484134808e47ed49b489b8205926b3
home_machine <home@email.com>
2018-08-22 23:17:22 +0300

    22.08.2018 * Работа над отчетами заказчиков

---
+++ Aura_DLL/Report.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace Aura.Model
+{
+    [Serializable]
+    public class Report
+    {
+        //класс, описывающий разные ежемесячные отчеты заказчиков
+        public Report(DataRow row)
+        {
+            organisationID = row[0] is DBNull ? 0 : (int)(long)row[0];
+            commonPurchasesContractsReport = row[1] is DBNull ? "" : (string)row[1];
+            singleSupplierContractsReport = row[2] is DBNull ? "" : (string)row[2];
+            failedPurchasesContractsReport = row[3] is DBNull ? "" : (string)row[3];
+
+
+        }
+
+
+        public int organisationID;      //ИД заказчика, для которого делается отчет
+
+        /// <summary>
+        /// каждый вид отчета хранится одной строкой для заказчика
+        /// Хранится в виде перечисления "Месяц-Год". Например
+        /// 01.2018,02.2018,03.2018
+        /// Если месяц год есть в строке - значит, отчет за этот месяц есть
+        /// </summary>
+        public string commonPurchasesContractsReport;       //договоры, заключенные по результатам закупок
+        public string singleSupplierContractsReport;        //договоры, заключенные у единственного поставщика
+        public string failedPurchasesContractsReport;       //договоры, заключенные по несостоявшимся процедурам
+
+      
+
+
+    }
+}
