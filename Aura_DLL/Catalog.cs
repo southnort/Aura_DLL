b8cb0a7bc99efa9ea0614adc43e35f86a4e76eee
home_machine <home@email.com>
2018-11-12 22:59:33 +0300

    12.11.2018 * Настройка DataSet для выгрузки в Excel

---
+++ Aura_DLL/Catalog.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace Aura.Model
+{
+    static public class Catalog
+    {
+        //класс-хранилище для списков в формах
+        public static List<string> allStatuses = new List<string>()
+        {
+            "<не указано>",         //0
+            "Опубликована",         //1
+
+            "Вскрытие конвертов",   //2
+            "Рассмотрение",         //3
+            "Оценка",               //4
+
+            "Первые части",         //5
+            "Вторые части",         //6
+            "Подведение итогов",    //7
+
+            "Завершена",            //8    
+            "Отменена",             //9
+
+        };
+
+
+        public static List<PurchaseMethod> purchaseMethods = new List<PurchaseMethod>()
+        {
+            //методы осуществления закупок
+            //статусы закупок тоже здесь
+            new EmptyPurchaseMethod(),  //0
+            new AloneProvider(),        //1
+            new DemandOfQuotation(),    //2
+            new DemandOfQuotationEF(),  //3
+            new Auction(),              //4
+            new Konkurs(),              //5
+            new KonkursEF(),            //6                
+            new AuctionEF(),            //7
+
+        };
+
+        public static List<string> protocolStatuses = new List<string>()
+        {
+            //статусы протоколов
+            "<не указано>",     //0 
+            "0 заявок",         //1
+            "1 заявка",         //2
+            "2 и более заявок", //3
+            "Жду скан",         //4
+            "Опубликовано",     //5
+
+        };
+
+        public static List<string> contractConditions = new List<string>
+        {
+            //состояние заключенного договора с заказчиком на обслуживание
+            "<не указано>",
+            "действует",
+            "расторгнут",
+            "без договора",
+
+        };
+
+        public static List<string> contractOriginalConditions = new List<string>
+        {
+            //есть ли оригинал договора на обслуживание с заказчиком, или только копия
+            "<не указано>",
+            "оригинал",
+            "нет оригинала",
+            "подписан ЭЦП",
+            "без договора",
+
+        };
+
+        public static List<string> laws = new List<string>()
+        {
+            //названия законов
+            "<не указано>",
+            "44-ФЗ",
+            "223-ФЗ",
+            "44-ФЗ и 223-ФЗ",
+
+        };
+
+        public static List<string> contractTypes = new List<string>()
+        {
+            //типы договоров с заказчиками
+            "<не указано>",
+            "постоянный",
+            "разовый",
+        };
+
+        public static Dictionary<string, string> dataTableHeaders
+            = new Dictionary<string, string>()
+        {
+                //технические названия колонок в классах переводятся в русский текст
+                { "id", "id" },
+                { "employeID", "Ответственный за размещение" },
+
+        };
+    }
+
+}
---
+++ Aura_DLL/PurchaseMethod.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+
+namespace Aura.Model
+{
+    public abstract class PurchaseMethod
+    {
+        //класс, описывающий метод закупки
+
+
+        public string name { get; protected set; }
+        public Dictionary<int, string> purchaseStatuses;
+
+        protected void CreateDictionary(List<int> statusIndexes)
+        {
+            //добавляем в словарь все доступные статусы по указанным индексам
+            purchaseStatuses = new Dictionary<int, string>();
+            foreach (var item in statusIndexes)
+            {
+                purchaseStatuses.Add(item, Catalog.allStatuses[item]);
+
+            }
+
+        }
+
+
+    }
+
+    public class EmptyPurchaseMethod : PurchaseMethod
+    {
+        //пустой метод определения поставщика
+        public EmptyPurchaseMethod()
+        {
+            name = "<не указано>";
+            var statusIndexes = new List<int>() { 0, };
+            CreateDictionary(statusIndexes);
+        }
+
+    }
+
+    public class AloneProvider : PurchaseMethod
+    {
+        //Единственный поставщик
+        public AloneProvider()
+        {
+            name = "Единственный поставщик";
+            var statusIndexes = new List<int>()
+            {
+                0, 1, 8, 9,
+            };
+            CreateDictionary(statusIndexes);
+        }
+    }
+
+
+    public class DemandOfQuotation : PurchaseMethod
+    {
+        //запрос котировок
+        public DemandOfQuotation()
+        {
+            name = "Запрос котировок";
+            var statusIndexes = new List<int>()
+            {
+                0, 1, 2, 3, 4, 8, 9,
+            };
+            CreateDictionary(statusIndexes);
+        }
+    }
+
+    public class DemandOfQuotationEF : PurchaseMethod
+    {
+        //запрос котировок в электронной форме
+        public DemandOfQuotationEF()
+        {
+            name = "Запрос котировок в ЭФ";
+            var statusIndexes = new List<int>()
+            {
+                0, 1, 2, 3, 4, 8, 9,
+            };
+            CreateDictionary(statusIndexes);
+        }
+    }
+
+
+    public class Auction : PurchaseMethod
+    {
+        //Электронный аукцион
+        public Auction()
+        {
+            name = "Электронный аукцион";
+            var statusIndexes = new List<int>()
+            {
+                0, 1, 5, 6, 7, 8, 9,
+            };
+            CreateDictionary(statusIndexes);
+        }
+    }
+
+
+    public class Konkurs : PurchaseMethod
+    {
+        //Обычный конкурс
+        public Konkurs()
+        {
+            name = "Конкурс";
+            var statusIndexes = new List<int>()
+            {
+                0, 1, 2, 3, 4, 8, 9,
+            };
+            CreateDictionary(statusIndexes);
+        }
+    }
+
+    public class KonkursEF : PurchaseMethod
+    {
+        //Конкурс в электронной форме
+        public KonkursEF()
+        {
+            name = "Конкурс в электронной форме";
+            var statusIndexes = new List<int>()
+            {
+                0, 1, 2, 3, 4, 8, 9,
+            };
+            CreateDictionary(statusIndexes);
+        }
+    }
+
+    public class AuctionEF : PurchaseMethod
+    {
+        //Электронный аукцион
+        public AuctionEF()
+        {
+            name = "Открытый аукцион в электронной форме";
+            var statusIndexes = new List<int>()
+            {
+                0, 1, 5, 6, 7, 8, 9,
+            };
+            CreateDictionary(statusIndexes);
+        }
+    }
+
+
+}
+
